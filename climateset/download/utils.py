aa89ff6c4a70360975b7a122fbb994b594003c8e
liellnima <julia.kaltenborn@mail.mcgill.ca>
2024-10-26 02:15:32 +0200

    remove get_selected_scenario as it is too restricting

--- climateset/download/utils.py
+++ climateset/download/utils.py
@@ -8,7 +8,7 @@ from typing import Union
 import pandas as pd
 import xarray as xr
 
-from climateset import APP_ROOT, RAW_DATA
+from climateset import RAW_DATA
 from climateset.utils import create_logger
 
 LOGGER = create_logger(__name__)
@@ -273,21 +273,3 @@ def _handle_base_search_constraints(ctx, default_frequency, default_grid_label):
     if frequency:
         ctx = ctx.constrain(frequency=frequency)
     return ctx
-
-
-def get_select_model_scenarios(path_to_file: Union[str, pathlib.Path] = None) -> pd.DataFrame:
-    """
-    This function returns a dataframe based on input Json file.
-
-    Args:
-        path_to_file: Path to Json file
-
-    Returns:
-        Dataframe
-    """
-    if not path_to_file:
-        path_to_file = APP_ROOT / "download/constants/selected_scenariosMIPs.json"
-    if isinstance(path_to_file, str):
-        path_to_file = pathlib.Path(path_to_file)
-    selected_scenarios = pd.read_json(path_to_file, orient="records")
-    return selected_scenarios
