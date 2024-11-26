e02b25b27afd793d5111331aa6e76f65879b96c5
f-PLT <fplt.softwaredeveloper@gmail.com>
2024-11-26 11:14:19 -0500

    Add generic step processor

--- climateset/processing/abstract_processor_step.py
+++ climateset/processing/abstract_processor_step.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from pathlib import Path
 
 
 class AbstractProcessorStep(ABC):
@@ -11,3 +12,12 @@ class AbstractProcessorStep(ABC):
 
     def get_results_directory(self):
         return self.results_directory
+
+
+def process_steps(input_directory: Path, list_of_steps: list[AbstractProcessorStep]):
+    current_input_directory = input_directory
+    for step in list_of_steps:
+        step.execute(current_input_directory)
+        # current_input_directory = step.get_results_directory()
+
+    return current_input_directory
