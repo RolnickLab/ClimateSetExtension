260952dd3e4a8232545f8f905b1431ae6808bfa3
f-PLT <fplt.softwaredeveloper@gmail.com>
2025-02-26 23:41:58 -0500

    Update download_from_config_file() to use existing functions for each Downloader

--- climateset/download/downloader.py
+++ climateset/download/downloader.py
@@ -1,21 +1,18 @@
+import logging
 import pathlib
 from typing import Union
 
-from climateset.download.cmip6_downloader import CMIP6Downloader
+from climateset.download.cmip6_downloader import cmip6_download_from_config
 from climateset.download.constants.esgf import CMIP6, INPUT4MIPS
-from climateset.download.downloader_config import (
-    AVAILABLE_CONFIGS,
-    create_cmip6_downloader_config_from_file,
-    create_input4mips_downloader_config_from_file,
-)
-from climateset.download.input4mips_downloader import Input4MipsDownloader
+from climateset.download.downloader_config import AVAILABLE_CONFIGS
+from climateset.download.input4mips_downloader import input4mips_download_from_config
 from climateset.download.utils import match_key_in_list
 from climateset.utils import create_logger, get_yaml_config
 
 LOGGER = create_logger(__name__)
 
 
-def download_from_config_file(config_file: Union[str, pathlib.Path]):
+def download_from_config_file(config_file: Union[str, pathlib.Path], logger: logging.Logger = LOGGER):
     """
     This function downloads variables automatically from input config file
     Args:
@@ -27,8 +24,8 @@ def download_from_config_file(config_file: Union[str, pathlib.Path]):
     config_dict = get_yaml_config(config_file)
 
     downloader_factory = {
-        INPUT4MIPS: {"configs": create_input4mips_downloader_config_from_file, "downloader": Input4MipsDownloader},
-        CMIP6: {"configs": create_cmip6_downloader_config_from_file, "downloader": CMIP6Downloader},
+        INPUT4MIPS: input4mips_download_from_config,
+        CMIP6: cmip6_download_from_config,
     }
 
     verified_config_keys = []
@@ -36,8 +33,11 @@ def download_from_config_file(config_file: Union[str, pathlib.Path]):
         verified_key = match_key_in_list(input_key=config_key, key_list=AVAILABLE_CONFIGS)
         if verified_key:
             verified_config_keys.append(verified_key)
+        else:
+            logger.error(
+                f"Input project [{config_key}] from [{config_file}]was not found in available projects. "
+                "Removing it from download list"
+            )
 
     for config_key in verified_config_keys:
-        configs = downloader_factory[config_key]["configs"](config_file=config_file)
-        downloader = downloader_factory[config_key]["downloader"](config=configs)
-        downloader.download()
+        downloader_factory[config_key](config=config_file)
